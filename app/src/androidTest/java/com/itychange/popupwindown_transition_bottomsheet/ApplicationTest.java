2c8c4ef57f7106f2a699053870f9021e14eb0bdd
Do Van Hoang <dovanhoang.it@gmail.com>
2015-12-18 16:08:35 +0700

    Initial commit

---
+++ app/src/androidTest/java/com/itychange/popupwindown_transition_bottomsheet/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.itychange.popupwindown_transition_bottomsheet;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/itychange/popupwindown_transition_bottomsheet/MainActivity.java
@@ -0,0 +1,46 @@
+package com.itychange.popupwindown_transition_bottomsheet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.PopupWindow;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      final  Button btn= (Button) findViewById(R.id.openpopup);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                LayoutInflater layoutInflater
+                        = (LayoutInflater)getBaseContext()
+                        .getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = layoutInflater.inflate(R.layout.popup_windown, null);
+                final PopupWindow popupWindow = new PopupWindow(
+                        popupView,
+                        LayoutParams.WRAP_CONTENT,
+                        LayoutParams.WRAP_CONTENT);
+
+                Button btnDismiss = (Button)popupView.findViewById(R.id.dismiss);
+                btnDismiss.setOnClickListener(new Button.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        // TODO Auto-generated method stub
+                        popupWindow.dismiss();
+                    }});
+
+
+                popupWindow.setAnimationStyle(R.anim.style);
+                popupWindow.showAsDropDown(btn, 50, -30);
+
+            }});
+    }
+}
